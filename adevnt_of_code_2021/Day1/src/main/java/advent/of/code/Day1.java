040ebe7ced130829a5ea278a7493dc4bf782fc47
madalina-lavinia-chelu <madalina.lavinia.chelu@gmail.com>
2022-11-30 22:55:24 +0200

    add day1

---
+++ adevnt_of_code_2021/Day1/src/main/java/advent/of/code/Day1.java
@@ -0,0 +1,50 @@
+package advent.of.code;
+
+import jdk.jfr.DataAmount;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+
+import java.io.File;
+import java.util.List;
+
+
+
+
+@Data
+@AllArgsConstructor
+public class Day1 {
+
+    static List<Integer> nums;
+
+    public Day1(File input) {
+        nums = staticUtils.Utlis.inputFileToIntList(input);
+    }
+
+    public int run1() {
+        int k = 0;
+        for(int i = 1; i < nums.size() - 1; i++)
+            if(nums.get(i) < nums.get(i+1))
+                k = k + 1;
+
+        if(nums.get(nums.size() - 2) < nums.get(nums.size() - 1))
+            k = k + 1;
+        return k;
+    }
+
+
+    public int run2(){
+        int k = 0;
+        for(int i = 0; i < nums.size() - 3; i++) {
+            int sumA = nums.get(i) + nums.get(i + 1) + nums.get(i + 2);
+            int sumB = nums.get(i + 1) + nums.get(i + 2) + nums.get(i + 3);
+
+            if(sumB > sumA) {
+                k++;
+            }
+        }
+        return k;
+    }
+
+
+}
---
+++ adevnt_of_code_2021/Day1/src/main/java/advent/of/code/Main.java
@@ -0,0 +1,15 @@
+package advent.of.code;
+
+import java.io.File;
+
+import static advent.of.code.Day1.nums;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Day1 test = new Day1(new File("C:\\Users\\Lavinia\\Desktop\\advent of code\\adevnt_of_code_2021\\Day1\\src\\main\\resources\\date.txt"));
+
+        System.out.println(test.run2());
+
+    }
+}
