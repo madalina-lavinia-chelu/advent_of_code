b4a7c507f104b8b36d135fe76c033ec14a26d7af
madalina-lavinia-chelu <madalina.lavinia.chelu@gmail.com>
2023-12-03 13:59:47 +0200

    add new files

---
+++ C++/advent_of_code_2015/Day5/Day5Part2.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <fstream>
+#include <cstring>
+using namespace std;
+ifstream fin ("bac.txt");
+int repeat (char s[])
+{
+    for(int i=1; i<strlen(s); i++)
+    {
+        if(s[i-1]==s[i+1])
+            return 1;
+    }
+    return 0;
+}
+int dublu (char s[])
+{
+    for(int i=0; i<strlen(s); i++)
+    {
+        for(int j=i+2; j<strlen(s); j++)
+        {
+            if(strncmp(s+i,s+j,2)==0)
+                return 1;
+        }
+    }
+    return 0;
+}
+int main()
+{
+    char s[100];
+    int n=0;
+    while(fin>>s)
+    {
+        if(repeat(s)&& dublu(s))
+            n++;
+    }
+    cout<< n;
+    return 0;
+}
