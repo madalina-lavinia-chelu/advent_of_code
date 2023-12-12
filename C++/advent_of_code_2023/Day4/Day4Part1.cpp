ddc69f676ac3e99f31c14858f95f7e63425560fe
madalina-lavinia-chelu <madalina.lavinia.chelu@gmail.com>
2023-12-12 18:44:10 +0200

    add new files

--- C++/advent_of_code_2023/Day4/Day4Part1.cpp
+++ C++/advent_of_code_2023/Day4/Day4Part1.cpp
@@ -34,41 +34,29 @@ int match(int s1[], int s2[]){
 
 int main(){
     char s[256] , *number;
-    long long int pt2 = 0;
+    long long int pt1 = 0;
 
     while(fin.getline(s, 256)){
-        int cards = 192;
-        int copies[193] = {1};
-        int valid = 0, x = 0, y = 0, pt2 = 0;
-        number = strtok(s, " ");
+        int valid = 0, pt2 = 0;
+        int s1[101] = {0}, s2[101] = {0},
         number = strtok(NULL, ":");
         number = strtok(NULL, " ");
 
-        int s1[101] = {0}, s2[101] = {0},
         while(number){
             if(strcmp(number, "|")==0)
                 valid = 1;
 
             if(valid == 0)
-                s1[++x] = convert(number);
+                s1convert(number)] ++ ;
             else if(strcmp(number, "|"))
-                s2[++y] = convert(number);
+                s2[convert(number)] ++;
             number = strtok(NULL, " ");
         }
-        for(int i = 1; i <= x; i++){
-            for(j = 1; j <= y; j++){
-                if(s1[i] == s2[j])
-                    nmatches++;
-                    pt2 += copies[i];
-                    copies[i+nmatches] += copies[i];
-                    break;
-            }
-        }
-
+        pt1 += doubleNumber(match(s1,s2));
 
     }
 
     cout << "--- Day 4: Scratchcards ---" << endl;
-    cout << "Part 1: " << pt2; //21213
+    cout << "Part 1: " << pt1; //21213
 
 }
